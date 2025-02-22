8621ab01d5a947cac465051ff1aa64a9d60a3bb3
cd28dev <carlos.adrianzen27972@gmail.com>
2025-02-22 01:46:49 -0500

    Implements BaseRepository

---
+++ src/main/java/com/cd/incidenciasappfx/repository/BaseRepository.java
@@ -0,0 +1,14 @@
+
+package com.cd.incidenciasappfx.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseRepository<T,ID> {
+    void save(T entity);
+    void update(T entity);
+    void deleteById(ID id);
+    Optional<T> findById(ID id);
+    List<T> findAll();
+    
+}
